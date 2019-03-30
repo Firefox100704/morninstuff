cff88c5921d078d9527aca9dd166e8e64df209b0
firefox100704 <jesus.robles100704@gmail.com>
2019-03-30 11:04:36 -0400

    Initial commit

---
+++ test.js
@@ -0,0 +1,24 @@
+$(document).ready(function(){
+// all jQuery method
+//$().click(function(){
+  //$("div").animate({left: '150px'});
+});
+
+// paragraph.innerHTML = "people can see this";
+// var videoBanner = document.getElementsById('videoBanner');
+// videoBanner.appendchild(paragraph);
+// var paragraph = document.createElements("p");
+
+// //new header 
+// var newHeader = document.createElements("h2");
+// newHeader.innerHTML = 'ladies and gentlemen';
+// videoBanner.insertbefore(newHeader,paragraph);
+
+// //removing stuff
+// videoBanner.removeChild('paragraph');
+// videoBanner.removeChild("newHeader");
+
+// videoBanner.addEventListener('click', myEventHandler)
+// function myEventHandler() {
+// 	alert( "hi how are you")
+
